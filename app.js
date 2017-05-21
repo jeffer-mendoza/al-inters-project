ef812a587f4d8cb067b14fc302043711997bd659
jeffer-mendoza <jefferson.mendoza@correounivalle.edu>
2017-05-21 13:06:32 -0500

    First example in AngularJs using the concepts of controller, directives, expressions.

---
+++ app.js
@@ -0,0 +1,17 @@
+(function(){
+
+var app = angular.module("app",[]);
+
+app.controller('StoreController', function(){
+
+this.product = gem;
+
+});
+
+var gem = {
+  name: 'Fast and Furious',
+  price: 2.95,
+  description: 'Action...'
+}
+
+})();

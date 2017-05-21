1020e0480fb95873c11037aa7f806fba8b0037c8
jeffer-mendoza <jefferson.mendoza@correounivalle.edu>
2017-05-21 13:30:35 -0500

    Example using the directives ng-show for show a element html and ng-repeat for show a list the objects with iterations

--- app.js
+++ app.js
@@ -4,14 +4,33 @@ var app = angular.module("app",[]);
 
 app.controller('StoreController', function(){
 
-this.product = gem;
+this.products = gems;
 
 });
 
-var gem = {
+var gems =[
+{
   name: 'Fast and Furious',
   price: 2.95,
-  description: 'Action...'
-}
+  description: 'Action...',
+  purchase: true,
+  soldout: false
+},
+{
+  name: 'Batman 3',
+  price: 2.95,
+  description: 'Action...',
+  purchase: false,
+  soldout: false
+},
+{
+  name: 'Goal 2',
+  price: 2.95,
+  description: 'Action...',
+  purchase: true,
+  soldout: true
+},
+
+]
 
 })();

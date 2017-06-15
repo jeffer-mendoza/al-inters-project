505d2fefb4883c6512759f71ed0df4fb70364468
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-06-15 09:28:10 -0500

    Moving components files to respective directories for group each of the functionalities

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 
-import { AppComponent } from './hero.component';
+import { AppComponent } from './hero.component/hero.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component.ts';
-import { HeroComponent }        from './hero.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent } from './app.component';
+import { HeroComponent }        from './hero.component/hero.component';
+import { HeroDetailComponent } from './hero-detail.component/hero-detail.component';
 
 @NgModule({
   imports: [

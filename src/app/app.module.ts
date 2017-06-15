aaad81ef7d3be1695ba61866a17052b3a358c6c1
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-06-15 12:48:16 -0500

    Adding DasdBoard Component for show the top5's heroes and RouteModule with 3 routes for dashboard, dashboard redirect and heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeroComponent }        from './hero.component/hero.component';
 import { HeroDetailComponent } from './hero-detail.component/hero-detail.component';
+import { DashBoardComponent } from './dashboard.component/dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashBoardComponent },
+      { path: 'heroes', component: HeroComponent }
+    ])
   ],
   declarations: [
     AppComponent,
     HeroComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    DashBoardComponent
   ],
   bootstrap: [ AppComponent ]
 })

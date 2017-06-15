4d95e31965eb9bb4e7914eea37d76375dfd8f2ed
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-06-15 15:45:12 -0500

    Refactoring Routing in new file app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeroComponent }        from './hero.component/hero.component';
 import { HeroDetailComponent } from './hero-detail.component/hero-detail.component';
 import { DashBoardComponent } from './dashboard.component/dashboard.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashBoardComponent },
-      { path: 'heroes', component: HeroComponent },
-      { path: 'detail/:id', component: HeroDetailComponent}
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -25,6 +22,7 @@ import { DashBoardComponent } from './dashboard.component/dashboard.component';
     HeroDetailComponent,
     DashBoardComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }

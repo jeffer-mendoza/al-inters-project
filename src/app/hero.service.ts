cf097715c0fcbe1f4c5593da1529bbde9e940e31
Jefferson <mendosajefferson@gmail.com>
2017-06-16 15:15:10 +0000

    Updating hero details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,7 +9,7 @@ import { Hero } from './model/hero';
 export class HeroService {
 
   getHero(id: number): Promise<Hero> {
-  const url = '${this.heroesUrl}/${id}';
+  const url = `${this.heroesUrl}/${id}`;
   return this.http.get(url)
     .toPromise()
     .then(response => response.json().data as Hero)
@@ -30,6 +30,25 @@ getHeroes(): Promise<Hero[]> {
 private handleError(error: any): Promise<any> {
   console.error('An error occurred', error); // for demo purposes only
   return Promise.reject(error.message || error);
+}
+
+private headers = new Headers({'Content-Type': 'application/json'});
+
+update(hero: Hero): Promise<Hero> {
+  const url = `${this.heroesUrl}/${hero.id}`;
+  return this.http
+    .put(url, JSON.stringify(hero), {headers: this.headers})
+    .toPromise()
+    .then(() => hero)
+    .catch(this.handleError);
+}
+
+create(name: string): Promise<Hero> {
+  return this.http
+    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+    .toPromise()
+    .then(res => res.json().data as Hero)
+    .catch(this.handleError);
 }
 
 }

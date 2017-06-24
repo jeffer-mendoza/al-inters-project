410c305ef7d2f3abeebedbcc77c44e36835d7eaf
jeffer-mendoza <jefferson.mendoza@correounivalle.edu.co>
2017-06-24 23:03:00 +0000

    Adding file's settings for global variables

---
+++ my-app/src/app/settings.ts
@@ -0,0 +1,9 @@
+/*
+Class to add configuration variables that are required in the application
+to be accessed in any component
+*/
+export const settings = {
+    apiKey: '802cd9bec58e75474a66bfa717fd1106',
+    apiUrl: 'https:////api.themoviedb.org/',
+    apiImg: '//image.tmdb.org/t/p/w500'
+}
